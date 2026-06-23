a4cea76cf120ca2a3f69355890a4ee5555825543
odm275 <oscarmejaweb@gmail.com>
2026-06-23 11:11:06 -0500

    feat: prevent interactions when animation is ongoing

--- src/components/HandWave.tsx
+++ src/components/HandWave.tsx
@@ -2,24 +2,38 @@
 import { useState } from "react";
 import { type CSSProperties } from "react";
 
+import { cn } from "@/lib/utils";
+
 const ACTION_HAND_WAVE_DELAY = "0s"; // No delay when triggering with click
 export default function HandWave() {
   const [waveKey, setWaveKey] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(true);
 
   const onClick = () => {
-    setWaveKey(waveKey + 1);
+    if (isAnimating) {
+      return;
+    }
+
+    setIsAnimating(true);
+    setWaveKey((currentWaveKey) => currentWaveKey + 1);
   };
 
   return (
     <span
       key={waveKey} // Re-render the component to restart the animation
       onClick={onClick}
+      onAnimationStart={() => setIsAnimating(true)}
+      onAnimationEnd={() => setIsAnimating(false)}
       style={
         waveKey > 0
           ? ({ "--hand-wave-delay": ACTION_HAND_WAVE_DELAY } as CSSProperties)
           : undefined
       }
-      className="animate-hand-wave inline-block origin-bottom -rotate-50 cursor-pointer pl-4"
+      className={cn(
+        "animate-hand-wave inline-block origin-bottom -rotate-50 pl-4",
+        isAnimating && "pointer-events-none cursor-default",
+        !isAnimating && "cursor-pointer",
+      )}
     >
       ✋
     </span>
